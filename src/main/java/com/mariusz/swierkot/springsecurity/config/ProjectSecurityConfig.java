004ce9b9476829479f796d522efe6463365baff6
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-07-17 01:01:15 +0200

    add in-memory user details manager configuration

--- src/main/java/com/mariusz/swierkot/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/mariusz/swierkot/springsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.mariusz.swierkot.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,6 +21,21 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
