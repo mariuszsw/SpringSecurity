663cc9e46587c86da242379cb3f95f95eed6aa89
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-07-21 17:07:00 +0200

    remove unnecessary imports and clean up code

---
+++ src/main/java/com/mariusz/swierkot/springsecurity/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.mariusz.swierkot.springsecurity.repository;
+
+import com.mariusz.swierkot.springsecurity.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+
+
+}
