9a469e48741a22622d904a4041d681ca4492a62f
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-08-12 16:08:31 +0200

    refactored security configuration for endpoints, added specific authority checks for each endpoint

--- src/main/java/com/mariusz/swierkot/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/mariusz/swierkot/springsecurity/config/ProjectSecurityConfig.java
@@ -27,7 +27,11 @@ public class ProjectSecurityConfig {
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .ignoringRequestMatchers("/contact", "/register"))
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
+                        .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
+                        .requestMatchers("/myBalance").hasAnyAuthority("VIEWACCOUNT","VIEWBALANCE")
+                        .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
+                        .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
+                        .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
